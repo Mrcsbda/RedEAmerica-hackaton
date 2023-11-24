bf63eddceba7516a6f099ffe4a51167e85f70883
Mrcsbda <macbedoya@gmail.com>
2023-11-24 09:52:42 -0500

    print comments

--- src/pages/comments/Comments.jsx
+++ src/pages/comments/Comments.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./comments.scss"
 import { Avatar, Button, Card, CardContent, CardHeader, IconButton, TextField, Typography } from '@mui/material'
 import { red } from '@mui/material/colors';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { addCommentToDB } from '../../services/comments';
+import { addCommentToDB, getComments } from '../../services/comments';
 import Swal from 'sweetalert2';
 const Comments = () => {
   const { register, handleSubmit } = useForm();
   const { postId } = useParams()
   const { userLogged } = useSelector((state) => state.auth)
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState(false)
+
+  useEffect(() => {
+    getData()
+  }, [newComment])
   const addComment = async (data) => {
     const comment = {
       ...data,
@@ -22,116 +28,48 @@ const Comments = () => {
 
     const resp = await addCommentToDB(comment)
 
-    if(resp) {
+    if (resp) {
       Swal.fire(
         "Excelente!",
         "Haz hecho un aporte a esta publicación con exito!",
         "success"
-       );
+      );
+      setNewComment(!newComment)
     } else {
       Swal.fire(
         "Oops!",
         "Algo salio mal, intenta de nuevo!",
         "error"
-       );
+      );
     }
+  }
+  const getData = async () => {
+    const data = await getComments(postId)
+    setComments(data)
   }
 
   return (
     <article className='comments'>
       <section className='comments__container'>
         <div className='comments__cards-container'>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardHeader
-              className='comments__header'
-              title="Shrimp and Chorizo Paella"
-            />
-            <CardContent>
-              <Typography className='comments__text' variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to cook
-                together with your guests. Add 1 cup of frozen peas along with the mussels,
-                if you like.
-              </Typography>
-            </CardContent>
-          </Card>
+          {
+            comments.length > 0 ? comments?.map((comment, index) => (
+              <Card className='comments__card' key={index} sx={{ maxWidth: 345 }}>
+              <CardHeader
+                className='comments__header'
+                title={comment.userName}
+              />
+              <CardContent>
+                <Typography className='comments__text' variant="body2" color="text.secondary">
+                {comment.comment}
+                </Typography>
+              </CardContent>
+            </Card>
+            )) : (
+              <h1 className='comments__title-empty-state'>No hay comentarios disponibles aún para esta publicación</h1>
+            )
+          }
+
         </div>
         <div className='comments__input-container'>
           <h1 className='comments__title'>Agregar comentario</h1>
@@ -142,7 +80,8 @@ const Comments = () => {
               label="Agrega tu comentario"
               className='comments__input'
               multiline
-              {...register("comment", { required: true })}
+              placeholder='Maximo 290 caracteres'
+              {...register("comment", { required: true, maxLength: 290 })}
               type='text'
             />
             <Button type='submit' className='comments__button' variant="contained">Publicar</Button>
--- src/services/comments.js
+++ src/services/comments.js
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import { firebaseDB } from "../firebase/firebaseConfig";
 export const addCommentToDB = async (comment) => {
     try {
@@ -7,5 +7,24 @@ export const addCommentToDB = async (comment) => {
     } catch (error) {
         console.log(error);
         return false
+    }
+}
+
+export const getComments = async (id) => {
+    try {
+        const commentsRef = collection(firebaseDB, "comments");
+        const queryComments = await query(commentsRef, where("postId", "==", id))
+        const dataComments = await getDocs(queryComments);
+        let comments = []
+        dataComments?.forEach((doc) => {
+            comments.push({
+                id: doc.id,
+                ...doc.data()
+            });
+        })
+        return comments
+    } catch (error) {
+        console.log(error);
+        return error
     }
 }
